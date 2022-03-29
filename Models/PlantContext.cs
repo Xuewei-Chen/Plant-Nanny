374b6faf37c0a34a0412483e813fecc555c99596
sahil <sahil.patel1010@gmail.com>
2022-03-28 22:57:32 -0400

    Add project files.

---
+++ Models/PlantContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PlantNanny.API.Models
+{
+    public class PlantContext : DbContext
+    {
+        public PlantContext(DbContextOptions<PlantContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Plant> Plants { get; set; }
+    }
+}
---
+++ Repositories/IPlantRepository.cs
@@ -0,0 +1,29 @@
+﻿using PlantNanny.API.Models;
+
+namespace PlantNanny.API.Repositories
+{
+    /*
+     * Operations that can be perform on the database
+     */ 
+    public interface IPlantRepository
+    {
+        Task<IEnumerable<Plant>> Get();
+        /* Getter functions */
+        //Task<Plant> Get(Plant plant);
+        Task<Plant> GetbyID(int Id);
+        //Task<IEnumerable<Plant>> GetbyID(int Id);
+        Task<IEnumerable<Plant>> GetbyName(string Name);
+        Task<IEnumerable<Plant>> GetbySciName(string Sci_Name);
+        Task<IEnumerable<Plant>> GetbyRegion(string Region);
+        Task<IEnumerable<Plant>> GetbyName_Region(string _Name, string _Region);
+        Task<IEnumerable<Plant>> GetbySciName_Region(string _SciName, string _Region);
+        Task<IEnumerable<Plant>> GetbySearch(string? _Name, string? _SciName, string? _Region);
+
+
+
+        Task<Plant> Create(Plant plant);
+        Task Update(Plant plant);
+        Task Delete(int Id);
+
+    }
+}
